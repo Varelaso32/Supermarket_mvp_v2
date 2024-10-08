8b64f82ca3826da8fd7192bfd2c0400157c87bc0
Varelaso32 <samyst2013@hotmail.com>
2024-10-07 22:35:51 -0500

    Se agrega el IPayModeRepository

--- Supermarket-mvp-v2/Models/PayModeModels.cs
+++ Supermarket-mvp-v2/Models/PayModeModels.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
-namespace Supermarket_mvp_v2.Models
+namespace Supermarketmvp.Models //Samuel Varela Morales
 {
-    internal class PayModeModels
+    internal class PayModeModel
     {
-    }
+        [DisplayName("Pay Mode Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Pay Mode Name")]
+        [Required(ErrorMessage = "Pay Mode name is Required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Pay Mode name must be between 3 and 50  characters")]
+        public string Name { get; set; }
+
+        [DisplayName("Pay Mode Observartion")]
+        [Required(ErrorMessage = "Pay Mode observation is Required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Pay Mode observation must be between 3 and 200  characters")]
+        public string Observation { get; set; }
+
+    }//Fin de la clase
 }
